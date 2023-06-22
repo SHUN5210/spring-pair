a5c8c1846df3364a08e769eb6cdf398c2dfed075
id <id@AQ-0121>
2023-06-22 11:49:33 +0900

    yossi

---
+++ src/main/java/com/example/demo/SpringPairApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPairApplication.class, args);
+	}
+
+}
