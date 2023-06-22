119961e7c7da64dde58459e75fcb1f920d8b839c
id <id@AQ-0121>
2023-06-22 16:18:08 +0900

    aaa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,5 @@ INSERT INTO categories(name) VALUES('野菜');
 INSERT INTO users(email, password) VALUES('shuijinhan@chahan.jp', 'pairchahan');
 
 --食材
-
+INSERT INTO itemsList(category_id,name) VALUES(3,'納豆');
 --チャーハン
