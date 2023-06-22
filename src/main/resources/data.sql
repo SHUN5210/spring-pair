e7399d025b521ff49760eca49a845bb83bb679b6
id <id@AQ-0039>
2023-06-22 14:13:03 +0900

    あーい

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- カテゴリーテーブルデータ
+INSERT INTO categories(name) VALUES('肉');
+INSERT INTO categories(name) VALUES('魚介');
+INSERT INTO categories(name) VALUES('野菜');
+INSERT INTO users(mail, password) VALUES('shuijinhan@chahan.jp', 'pairchahan');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+-- テーブル削除
+DROP TABLE IF EXISTS chahan;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS categories;
+
+	-- カテゴリーテーブル
+CREATE TABLE categories
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT
+);
+-- 商品テーブル
+CREATE TABLE items
+(
+   id SERIAL PRIMARY KEY,
+   category_id INTEGER,
+   name TEXT,
+   detail TEXT
+);
+-- 注文テーブル
+CREATE TABLE chahan
+(
+  id SERIAL PRIMARY KEY,
+  menu TEXT,
+  name1 TEXT,
+  name2 TEXT,
+  name3 TEXT
+);
+-- 注文詳細テーブル
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY,
+  mail TEXT,
+  password TEXT
+); 
