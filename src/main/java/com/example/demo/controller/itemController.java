fe0ed5408610939fdd833c9982c5de424d8c69ac
id <id@AQ-0121>
2023-06-28 13:19:27 +0900

    aa

--- src/main/java/com/example/demo/controller/itemController.java
+++ src/main/java/com/example/demo/controller/itemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -175,4 +176,13 @@ public class itemController {
 		return "confirm";
 	}
 	
+	@GetMapping("/items/{id}/detail")
+	public String detail(
+			@PathVariable("id") Integer id,
+			Model m
+			) {
+		Item item =itemRepository.findById(id).get();
+		m.addAttribute("item",item);
+		return"detail";
+	}
 }
